5617addec5b93f9502746816f7182c75d7a5adce
Zac Baston <zbaston@gmail.com>
2017-06-07 09:43:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 function findMinAndRemoveSorted(array){
-  return array.shift()
+  return array.shift();
 }
 
 function merge(firstSubarray, secondSubArray){
-  let sorted = []
-  while(firstSubarray.length != 0 && secondSubArray.length != 0){
-    if(firstSubarray[0] < secondSubArray[0]){
-      sorted.push(findMinAndRemoveSorted(firstSubarray))
-    } else {
-      sorted.push(findMinAndRemoveSorted(secondSubArray))
-    }
+  let sorted = [];
+
+  while (firstSubarray.length && secondSubArray.length) {
+    firstSubarray[0] <= secondSubArray[0] ?
+          sorted.push(findMinAndRemoveSorted(firstSubarray)) :
+          sorted.push(findMinAndRemoveSorted(secondSubArray));
   }
-  return sorted.concat(firstSubarray).concat(secondSubArray)
-}
+
+  return sorted.concat(firstSubarray).concat(secondSubArray);
+ }
 
 function mergeSort(array){
-  let midpoint = array.length/2
-  let firstHalf = array.slice(0, midpoint)
-  let secondHalf = array.slice(midpoint, array.length)
-  let sorted;
+  if (array.length < 2) { return array };
 
-  if(array.length < 2){
-    return array
-  } else {
-    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
-  }
-  return sorted
+  let middle = parseInt(array.length / 2);
+  let leftArray = array.slice(0, middle);
+  let rightArray = array.slice(middle, array.length);
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
 }
