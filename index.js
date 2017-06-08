813561fb660f32bad167b8ef66883ccd20385967
Alex Griffith <alex.griffith@flatironschool.com>
2017-06-08 09:52:47 -0400

    Add solution to Merge Sort

--- index.js
+++ index.js
@@ -1,54 +1,29 @@
-function getName(node){
-  return node.name;
+function findMinAndRemoveSorted(array){
+  return array.shift()
 }
 
-function next(node, collection){
-  let nextAddress = node.next
-  return collection[`${nextAddress}`]
+function merge(firstSubarray, secondSubArray){
+  let sorted = []
+  while(firstSubarray.length != 0 && secondSubArray.length != 0){
+    if(firstSubarray[0] < secondSubArray[0]){
+      sorted.push(findMinAndRemoveSorted(firstSubarray))
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondSubArray))
+    }
+  }
+  return sorted.concat(firstSubarray).concat(secondSubArray)
 }
 
-function addressAt(index, linkedList, collection){
-  if(index == 0){
-    return linkedList
+function mergeSort(array){
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  let sorted;
+
+  if(array.length < 2){
+    return array
   } else {
-    let node = nodeAt(index-1, linkedList, collection)
-    return node.next
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
-}
-
-function headNode(linkedList, collection){
-  return collection[linkedList]
-}
-
-function nodeAt(index, linkedList, collection){
-  let currentNode = headNode(linkedList, collection);
-  for(let i = 0; i < index; i++){
-     currentNode = next(currentNode, collection);
-  }
-
-  return currentNode;
-}
-
-function indexAt(node, collection, linkedList){
-  let currentNode = headNode(linkedList, collection);
-  let currentIdx = 0
-  while(currentNode != node){
-    currentIdx++
-    currentNode = next(currentNode, collection)
-  }
-  return currentIdx
-}
-
-
-function insertNodeAt(index, newNodeAddress, linkedList, collection){
-  let previousNode = nodeAt(index - 1, linkedList, collection)
-  let subsequentNode = nodeAt(index, linkedList, collection)
-
-  let previousNodeIdx = indexAt(previousNode, collection, linkedList)
-  let subsequentNodeIdx = indexAt(subsequentNode, collection, linkedList)
-  let previousNodeAddress = addressAt(previousNode, linkedList, collection)
-  let subsequentNodeAddress = addressAt(subsequentNode, linkedList, collection)
-  previousNode.next = newNodeAddress
-  let newNode = collection[newNodeAddress]
-  newNode.next = subsequentNodeAddress
+  return sorted
 }
