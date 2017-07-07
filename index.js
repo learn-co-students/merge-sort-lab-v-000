63f313ee6ab64f9c04848858dadb32d24f3c33b2
SiamKing <thesiameseelectric@gmail.com>
2017-07-06 19:27:24 -0700

    finished

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function findMinAndRemoveSorted(arr) {
+  return arr.shift()
+}
 
+function merge(firstHalf, secondHalf) {
+  let sorted = [];
+
+  while (firstHalf.length != 0 && secondHalf.length != 0) {
+    if (firstHalf[0] < secondHalf[0]) {
+      sorted.push(findMinAndRemoveSorted(firstHalf));
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondHalf));
+    }
+  }
+  return sorted.concat(firstHalf).concat(secondHalf);
+}
+
+function mergeSort(arr) {
+  let midPoint = arr.length / 2,
+      firstHalf = arr.slice(0, midPoint),
+      secondHalf = arr.slice(midPoint),
+      sorted;
+
+  if (arr.length < 2) {
+    return arr;
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+   return sorted;
+}
