0ac3fd87f1437cb3f167d87131f8425bd05e2144
BRETT COLE <NeWdEsiGNs@BRETTs-MacBook-Pro.local>
2017-12-27 20:04:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+function findMinAndRemoveSorted(firstArr, secondArr) {
+  if (secondArr == undefined) {
+    firstArr.sort((a, b) => {
+      a - b;
+    })
+    return firstArr.splice(0, 1);
+  } else {
+    let minFirstHalf = firstArr[0];
+    let minSecondHalf = secondArr[0];
+    if (minFirstHalf < minSecondHalf) {
+      return firstArr.shift();
+    } else {
+      return secondArr.shift();
+    }
+  }
+}
 
+function merge(firstArr, secondArr) {
+  let sorted = [];
+  let currentMin;
+  while (firstArr.length != 0 && secondArr.length != 0) {
+    currentMin = findMinAndRemoveSorted(firstArr, secondArr)
+    sorted.push(currentMin)
+  }
+  return sorted.concat(firstArr).concat(secondArr);
+}
+
+function mergeSort(array) {
+  let midpoint = array.length / 2;
+  let firstHalf = array.slice(0, midpoint);
+  let secondHalf = array.slice(midpoint, array.length);
+  let sorted;
+  if (array.length < 2) {
+    return array;
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+  return sorted;
+}
