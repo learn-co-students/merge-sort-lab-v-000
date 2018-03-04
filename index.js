ad9241db19cf5dd5e42bdce562f3fb8fb810856f
github username <dsbondin@gmail.com>
2018-03-04 13:18:10 -0500

    Some formatting

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function merge(array1, array2) {
   let array = [];
   while (array1.length !== 0 && array2.length !== 0) {
     array1[0] < array2[0] ? array.push(findMinAndRemoveSorted(array1)) : array.push(findMinAndRemoveSorted(array2))
+    console.log("sorted array: ", array)
   }
+  console.log("concatenating ", array, array1, array2)
   return array.concat(array1).concat(array2)
 }
 
@@ -20,5 +22,7 @@ function mergeSort(array) {
     let array2 = array.slice(med, array.length)
     console.log("array2: ", array2)
     return merge(mergeSort(array1), mergeSort(array2))
-  } else {return array}
+  } else {
+    return array
+  }
 }
