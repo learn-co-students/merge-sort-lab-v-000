e185bea8a83bc608c71c3b5533acdd19e843295f
ndalcin <dalcin.nicole@gmail.com>
2018-03-20 17:21:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function findMinAndRemoveSorted(array){
+  return array.shift();
+}
 
+function merge(array1, array2){
+  let sorted = []
+  while (array1.length != 0 && array2.length != 0){
+    if (array1[0] < array2[0]){
+      sorted.push(findMinAndRemoveSorted(array1))
+    } else {
+      sorted.push(findMinAndRemoveSorted(array2))
+    }
+  }
+  return sorted.concat(array1).concat(array2);
+}
+
+function mergeSort(array){
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  let sorted;
+
+  if (array.length < 2){
+     return array
+   } else {
+     sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+   }
+   return sorted;
+}
