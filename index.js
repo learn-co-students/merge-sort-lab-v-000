590c208ed0dab352f4600056ec9edb7f7913ef33
gnappo1 <gnappo.1@live.it>
2018-04-03 23:32:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function findMinAndRemoveSorted(array) {
+  return array.shift();
+}
 
+function merge(firstSubarray, secondSubArray) {
+  let sorted = [];
+  while(firstSubarray.length !== 0 && secondSubArray.length !== 0) {
+    if (firstSubarray[0] < secondSubArray[0]) {
+      sorted.push(findMinAndRemoveSorted(firstSubarray));
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondSubArray));
+    }
+  }
+  return sorted.concat(firstSubarray).concat(secondSubArray)
+}
+
+function mergeSort(unsorted) {
+  let median = unsorted.length/2,
+      firstHalf = unsorted.slice(0, median),
+      secondHalf = unsorted.slice(median, unsorted.length),
+      sorted;
+  if (unsorted.length < 2) {
+    return unsorted;
+  } else {
+     sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+  return sorted;
+}
