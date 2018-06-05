9c2982f3f59132a7f6301ced26f27c21e10aa9aa
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-06-05 01:17:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function findMinAndRemoveSorted(array) {
+    let min = array[0];
+    let minIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minIndex = i;
+        }
+    }
+    array = array.splice(minIndex, 1);
+    return min;
+}
 
+function merge(firstArray, secondArray) {
+    let sorted = [];
+    let currMin;
+    while (firstArray.length != 0 && secondArray.length != 0) {
+        if (firstArray[0] <= secondArray[0]) {
+            let currMin = findMinAndRemoveSorted(firstArray);
+        } else {
+            let currMin = findMinAndRemoveSorted(secondArray);
+        }
+        sorted.push(currMin);
+    }
+    return sorted.concat(firstArray).concat(secondArray);
+}
