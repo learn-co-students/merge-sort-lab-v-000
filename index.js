0d2286619a2c6c55d1ac20fe3f1bf41517201f7d
Michael Casciato <casciatom@gmail.com>
2018-06-21 21:50:27 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function findMinAndRemoveSorted(array) {
+  return array.shift();
+}
 
+function merge(firstHalf, secondHalf){
+  let sorted = []
+  let currentMin;
+  while(firstHalf.length != 0 && secondHalf.length != 0){
+		let minFirstHalf = firstHalf[0]
+		let minSecondHalf = secondHalf[0]
+
+		if (minFirstHalf < minSecondHalf) {
+			currentMin = firstHalf.shift()
+		} else {
+			currentMin = secondHalf.shift()
+		}
+		sorted.push(currentMin)
+  }
+  return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array){
+	let midpoint = array.length / 2
+	let firstHalf = array.slice(0, midpoint)
+	let secondHalf = array.slice(midpoint)
+
+	if (array.length < 2) {
+		return array
+	} else {
+		return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+	}
+}
