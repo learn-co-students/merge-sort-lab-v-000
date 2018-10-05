b2482e46901d9bfb6b6e60adcbeeb333ea9beec0
urnotjessie <zg2220@columbia.edu>
2018-10-05 10:57:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function findMinAndRemoveSorted(myArray) {
+    return myArray.shift();
+}
 
+function merge(firstSubarray, secondSubarray) {
+    let sorted = [];
+    while (firstSubarray.length != 0 && secondSubarray.length != 0) {
+
+        if (firstSubarray[0] < secondSubarray[0]) {
+            sorted.push(findMinAndRemoveSorted(firstSubarray))
+        } else {
+            sorted.push(findMinAndRemoveSorted(secondSubarray))
+        }
+    }
+    return sorted.concat(firstSubarray).concat(secondSubarray)
+}
+
+function mergeSort(unsortedArray) {
+    let middleIndex = unsortedArray.length / 2;
+    let firstHalf = unsortedArray.slice(0, middleIndex);
+    let secondHalf = unsortedArray.slice(middleIndex, unsortedArray.length)
+    let sorted
+    if (unsortedArray.length < 2) {
+        return unsortedArray
+    } else {
+        sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+
+    return sorted
+}
+
+console.log(mergeSort( [12, 10, 9, 14, 1, 3, 5, 11, 6, 15, 16, 13, 2, 4, 8, 7]))
