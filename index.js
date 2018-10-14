c445b6861e9c1e8a8fb9ba37c2b9e03224c90eb2
jlmack13 <jennimacklin@gmail.com>
2018-10-14 15:35:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift();
+}
 
+function merge(firstSub, secondSub) {
+    let sorted = [];
+    while(firstSub.length !== 0 && secondSub.length !== 0) {
+        if(firstSub[0] < secondSub[0]) {
+            sorted.push(findMinAndRemoveSorted(firstSub));
+        } else {
+            sorted.push(findMinAndRemoveSorted(secondSub));
+        }
+    }
+    return sorted.concat(firstSub).concat(secondSub);
+}
+
+function mergeSort(array) {
+    let midpoint = array.length/2;
+    let firstHalf = array.slice(0, midpoint);
+    let secondHalf = array.slice(midpoint, array.length);
+    let sorted;
+
+    if(array.length < 2) {
+        return array;
+    } else {
+        sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf));
+    }
+    return sorted;
+}
