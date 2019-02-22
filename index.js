bfc26dce8053c4ffab1678053e1c6896c1daf56b
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-22 15:47:17 -0800

    Define/complete merge()

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function findMinAndRemoveSorted(arr) {
   return arr.shift()
 }
+
+function merge(arr1, arr2) {
+  let sorted = []
+  while(arr1.length && arr2.length ) {
+    if (arr1[0] <= arr2[0]) {
+     sorted.push(findMinAndRemoveSorted(arr1))
+   } else {
+     sorted.push(findMinAndRemoveSorted(arr2))
+   }
+  }
+  return sorted.concat(arr1, arr2)
+}
