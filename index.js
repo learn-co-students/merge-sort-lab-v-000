48abbdf6c279fba59de8b098d80632ca3fd0fae8
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-22 17:50:01 -0800

    Complete mergeSort() w/ recursive merge()

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function merge(arr1, arr2) {
   }
   return sorted.concat(arr1, arr2)
 }
+
+function mergeSort(arr) {
+  const length = arr.length
+  if (length < 2) {
+    return arr
+  }
+  const middle = Math.floor(length / 2)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
