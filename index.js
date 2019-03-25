c8b01420c5809f054d7dbdfa5cf8dd1b3d49d376
Paris Dolfman <parisdolfman@gmail.com>
2019-03-25 20:01:05 +0000

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,17 @@
      }
    return sorted.concat(firstHalf).concat(secondHalf)
  }
+
+
+function mergeSort(array) {
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  let sorted;
+  if (array.length < 2) {
+    return array
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+  return sorted
+}
