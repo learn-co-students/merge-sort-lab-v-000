0eaa8aa3caf961793d97dd6969d891839f71ae84
Paris Dolfman <parisdolfman@gmail.com>
2019-03-25 19:48:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
- function findMinAndRemoveSorted(firstHalf, secondHalf) {
-   let minfirstHalf = firstHalf[0]
-   let minsecondHalf = secondHalf[0]
-   if (minfirstHalf < minsecondHalf) {
-     return firstHalf
-   } else {
-     return secondHalf
-   }
+
+// let array = [2, 3, 4, 5, 6, 7]
+
+ function findMinAndRemoveSorted(array) {
+   return array.shift()
+ }
+
+ function merge(firstHalf, secondHalf) {
+   let sorted = []
+   let currentMin;
+   while (firstHalf.length != 0 && secondHalf.length != 0) {
+     if (firstHalf[0] < secondHalf[0]) {
+       sorted.push(findMinAndRemoveSorted(firstHalf))
+     } else {
+       sorted.push(findMinAndRemoveSorted(secondHalf))
+     }
+     }
+   return sorted.concat(firstHalf).concat(secondHalf)
  }
