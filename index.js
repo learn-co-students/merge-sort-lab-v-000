9c9ca5d9cfb7de215342954787c1776fbd7d58c8
Jack Yeung <jyeung.se@gmail.com>
2019-03-31 21:50:55 -0400

    Created merge function.

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const findMinAndRemoveSorted = (array) => {
   return array.shift()
 }
 
-const merge = () => {
-
+const merge = (firstSubarray, secondSubArray) => {
+  let sorted = []
+  while (firstSubarray.length != 0 && secondSubArray.length != 0) {
+    if(firstSubarray[0] < secondSubArray[0]) {
+      sorted.push(findMinAndRemoveSorted(firstSubarray))
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondSubArray))
+    }
+  }
+  return sorted.concat(firstSubarray).concat(secondSubArray)
 }
 
 const mergeSort = () => {
-
+  
 }
