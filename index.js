3a4d7ca0982ad786d79be10173046c6fe2e31639
Jenny Kam <jkam17@gmail.com>
2019-04-09 16:21:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+function findMinAndRemoveSorted(a){
+  return a.shift()
+}
+
+function merge(a1, a2){
+  let sorted=[]
+  while (a1.length > 0 && a2.length > 0){
+    if (a1[0] <= a2[0]){
+      sorted.push(a1.shift())
+    }
+    else{
+      sorted.push(a2.shift())
+    }
+  }
+  return sorted.concat(a1).concat(a2)
+}
+
+function mergeSort(a){
+  let mid = a.length/2
+  let a1 = a.slice(0,mid)
+  let a2 = a.slice(mid)
+
+  if (a.length < 2){
+    return a
+  }
+  else {
+    return merge(mergeSort(a1), mergeSort(a2))
+  }
+}
