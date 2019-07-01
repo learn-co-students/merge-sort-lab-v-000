2da6918691998abb1835aef780d1920d44a33782
Rygel-XVI <micha232@umn.edu>
2019-07-01 15:47:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+function findMinAndRemoveSorted(array) {
+  return array.shift()
+}
+
+
+function merge(first, second) {
+  let sorted = []
+  while (first.length != 0 && second.length != 0) {
+    if (first[0] < second[0]) {
+      sorted.push(findMinAndRemoveSorted(first))
+    } else {
+      sorted.push(findMinAndRemoveSorted(second))
+    }
+  }
+
+ return sorted.concat(first).concat(second)
+}
+
+
+function mergeSort(array) {
+  let midpoint = array.length/2
+  let first = array.slice(0, midpoint)
+  let second = array.slice(midpoint, array.length)
+
+  if (array.length < 2) {
+    return array
+  } else {
+    return merge(mergeSort(first), mergeSort(second))
+  }
+}
