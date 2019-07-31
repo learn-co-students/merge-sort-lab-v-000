563a839a57613f7a596920e96550478aeb9292d7
nichia <nichia@gmail.com>
2019-07-31 15:18:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function findMinAndRemoveSorted(array) {
+  return array.shift();
+}
 
+function merge(firstSubarray, secondSubArray) {
+  let sortedArray = [];
+  while (firstSubarray.length > 0 && secondSubArray.length > 0) {
+    if (firstSubarray[0] < secondSubArray[0]) {
+      sortedArray.push(findMinAndRemoveSorted(firstSubarray));
+    } else {
+      sortedArray.push(findMinAndRemoveSorted(secondSubArray));
+    }
+  }
+  return sortedArray.concat(firstSubarray).concat(secondSubArray);
+}
+
+function mergeSort(array) {
+  let sorted;
+  let midPoint = array.length / 2;
+  let firstHalf = array.slice(0, midPoint);
+  let secondHalf = array.slice(midPoint, array.length);
+
+  if (array.length < 2) {
+    return array;
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf));
+    return sorted;
+  }
+}
