9c52ff2a2a78d67c60854bfb1ca2ddad6c5160de
David McGrann <dpmcgrann@gmail.com>
2019-10-12 16:24:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const findMinAndRemoveSorted = (array) => {
+  return array.shift()
+}
 
+const merge = (firstHalf, secondHalf) => {
+  let sorted = []
+  
+  while(firstHalf.length != 0 && secondHalf.length != 0){
+    if(firstHalf[0] < secondHalf[0]){
+      sorted.push(findMinAndRemoveSorted(firstHalf))
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondHalf))
+    }
+  }
+  return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+const mergeSort= (array) => {
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  let sorted
+
+  if(array.length < 2){
+    return array
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+  return sorted
+}
