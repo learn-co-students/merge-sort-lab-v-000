39f87e1e0df5669e9129e34d60bcdaa969869756
Ryan <ryanwevans@icloud.com>
2019-12-18 09:52:45 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+function findMinAndRemoveSorted(array) {
+    let minNum = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++){
+        if(array[i] < minNum){
+        minNum = array[i]
+        minIndex = i
+        }
+    }
+    array.splice(minIndex, 1);
+    return minNum;
+}
 
+function merge(array1, array2) {
+    let sortedArray = [];
+    let arraysLength = array1.length + array2.length;
+    while (array1.length != 0 && array2.length != 0) {
+        if (array1[0] < array2[0]) {
+            sortedArray.push(array1.shift())
+        } else {
+            sortedArray.push(array2.shift())
+        }
+    }
+    return sortedArray.concat(array1).concat(array2);
+}
+
+// arr1 = [1, 2, 5, 8];
+// arr2 = [3, 4, 6, 7];
+// console.log(merge(arr1, arr2));
+
+
+function mergeSort(array) {
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    let sortedArray;
+   
+    if (array.length < 2) {
+      return array
+    } else {
+      sortedArray = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return sortedArray;
+}
