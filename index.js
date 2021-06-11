a81dce34d002665bd922e8abe750d141466ea619
Dani <dani8439@aol.com>
2021-06-11 14:47:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function findMinAndRemoveSorted(array) {
+  return array.shift();
+}
 
+function merge(firstSubarray, secondSubarray) {
+  let sorted = [];
+  while (firstSubarray.length != 0 && secondSubarray.length != 0) {
+    if (firstSubarray[0] < secondSubarray[0]) {
+      sorted.push(findMinAndRemoveSorted(firstSubarray));
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondSubarray));
+    }
+  }
+  return sorted.concat(firstSubarray).concat(secondSubarray);
+}
+
+function mergeSort(array) {
+  let midpoint = array.length / 2;
+  let firstHalf = array.slice(0, midpoint);
+  let secondHalf = array.slice(midpoint, array.length);
+
+  if (array.length < 2) {
+    return array;
+  } else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+}
